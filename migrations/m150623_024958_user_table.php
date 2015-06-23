1834c5f6c114589aa9a9d25ae9a6f7c3c3710589
dawei <daweili@zju.edu.cn>
2015-06-23 15:30:24 +0800

    migrate user

--- migrations/m150623_024958_user_table.php
+++ migrations/m150623_024958_user_table.php
@@ -5,16 +5,31 @@ use yii\db\Migration;
 
 class m150623_024958_user_table extends Migration
 {
-    public function up()
+    public function safeUp()
     {
+    	$this->createTable('user', [
+    		'id' => Schema::TYPE_PK,
+    		'user' => Schema::TYPE_STRING . '(20) NOT NULL',
+    		'pwd' => Schema::TYPE_STRING . ' NOT NULL',
+    		'nickname' => Schema::TYPE_STRING . '(20) ',
+    		'thumb' => Schema::TYPE_STRING,
+    		'email' => Schema::TYPE_STRING . ' NOT NULL',
+    		'gender' => Schema::TYPE_STRING,
+    		'area' => Schema::TYPE_STRING,
+    		'job' => Schema::TYPE_STRING,
+    		'hobby' => Schema::TYPE_STRING,
+    		'signature' => Schema::TYPE_STRING,
+    	]);
+    	$this->createIndex('user', 'user', 'user',true);
+    	$this->createIndex('email', 'user', 'email',true);
 
     }
 
-    public function down()
+    public function safeDown()
     {
-        echo "m150623_024958_user_table cannot be reverted.\n";
-
-        return false;
+        //echo "m150623_024958_user_table cannot be reverted.\n";
+		$this->dropTable('user');
+        //return false;
     }
     
     /*
