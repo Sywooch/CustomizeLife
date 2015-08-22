a6bd93ae7a3a0a4bf09022f7b56b2143a2854236
JackXuF <244153559@qq.com>
2015-08-22 18:45:35 +0800

    merge

--- modules/v1/controllers/MessageController.php
+++ modules/v1/controllers/MessageController.php
@@ -11,6 +11,9 @@ use app\modules\v1\models\User;
 use yii\data\ActiveDataProvider;
 use app\modules\v1\models\Reply;
 
+require dirname ( dirname ( dirname ( __FILE__ ) ) ) . '/../vendor/pushserver/sdk.php';
+use PushSDK;
+
 class MessageController extends ActiveController {
 	public $modelClass = 'app\modules\v1\models\Message';
 	public $serializer = [ 
@@ -23,57 +26,85 @@ class MessageController extends ActiveController {
 		unset ( $actions ['view'] );
 		return $actions;
 	}
-// 	public function actionTest() {
-// 		// $sql = "SELECT * FROM table WHERE cid=2 and status=1";
-// 		$criteria = new CDbCriteria ();
-// 		$result = $msg = Message::find ()->select ( 'msg.id' )->join ( 'INNER JOIN', 'friends', 'friends.friendid = msg.userid and friends.myid=:id',[ ':id' => Yii::$app->user->id ] );
-// 		$pages = new CPagination ( $result->rowCount );
-// 		$pages->pageSize = 10;
-// 		$pages->applyLimit ( $criteria );
-// 		$result = $result->offset ( $pages->currentPage * $pages->pageSize )->limit ( $pages->pageSize );
-// 		// $result=Yii::app()->db->createCommand($sql." LIMIT :offset,:limit");
-// 		// $result->bindValue(':offset', $pages->currentPage*$pages->pageSize);
-// 		// $result->bindValue(':limit', $pages->pageSize);
-// 		$posts = $result->query ();
-// 		return $posts->all ();
-// 	}
-	public function actionId(){
+	// public function actionTest() {
+	// // $sql = "SELECT * FROM table WHERE cid=2 and status=1";
+	// $criteria = new CDbCriteria ();
+	// $result = $msg = Message::find ()->select ( 'msg.id' )->join ( 'INNER JOIN', 'friends', 'friends.friendid = msg.userid and friends.myid=:id',[ ':id' => Yii::$app->user->id ] );
+	// $pages = new CPagination ( $result->rowCount );
+	// $pages->pageSize = 10;
+	// $pages->applyLimit ( $criteria );
+	// $result = $result->offset ( $pages->currentPage * $pages->pageSize )->limit ( $pages->pageSize );
+	// // $result=Yii::app()->db->createCommand($sql." LIMIT :offset,:limit");
+	// // $result->bindValue(':offset', $pages->currentPage*$pages->pageSize);
+	// // $result->bindValue(':limit', $pages->pageSize);
+	// $posts = $result->query ();
+	// return $posts->all ();
+	// }
+	public function actionId() {
 		$data = Yii::$app->request->post ();
-		$id = $data['id'];
-		$msg = (new \yii\db\Query())->select(['msg.*','user.nickname','user.thumb'])->from ('msg')->join('INNER JOIN', 'user','msg.userid = user.id and msg.id = :id',[':id'=>$id])->one ();
-		$info=$msg;
-		$info['apps'] = (new \yii\db\Query())->
-		select ( [
+		$id = $data ['id'];
+		$msg = (new \yii\db\Query ())->select ( [ 
+				'msg.*',
+				'user.nickname',
+				'user.thumb' 
+		] )->from ( 'msg' )->join ( 'INNER JOIN', 'user', 'msg.userid = user.id and msg.id = :id', [ 
+				':id' => $id 
+		] )->one ();
+		$info = $msg;
+		$info ['apps'] = (new \yii\db\Query ())->select ( [ 
 				'msgtoapp.appid',
-				'app.icon'
-				] )->from ( 'msgtoapp' )->join ( 'INNER JOIN', 'app', 'msgtoapp.appid = app.id and msgtoapp.msgid = :id',[':id'=>$id])->all();
-		$info['replys'] = (new \yii\db\Query())->select(['reply.*','user1.nickname as fromnickname','user2.nickname as tonickname'])->from ( 'reply' )->join('INNER JOIN','user user1','user1.id = reply.fromid and reply.msgid= :id',[':id'=>$id ])->join('Left JOIN','user user2','user2.id = reply.toid')->orderBy("reply.created_at")->all();
+				'app.icon' 
+		] )->from ( 'msgtoapp' )->join ( 'INNER JOIN', 'app', 'msgtoapp.appid = app.id and msgtoapp.msgid = :id', [ 
+				':id' => $id 
+		] )->all ();
+		$info ['replys'] = (new \yii\db\Query ())->select ( [ 
+				'reply.*',
+				'user1.nickname as fromnickname',
+				'user2.nickname as tonickname' 
+		] )->from ( 'reply' )->join ( 'INNER JOIN', 'user user1', 'user1.id = reply.fromid and reply.msgid= :id', [ 
+				':id' => $id 
+		] )->join ( 'Left JOIN', 'user user2', 'user2.id = reply.toid' )->orderBy ( "reply.created_at" )->all ();
 		return $info;
 	}
 	public function actionGet() {
-		$data=Yii::$app->request->post();
-		$phone=User::findOne([
-				'phone'=>$data['phone']
-		]);
-		//$data = Message::find ()->select ( 'msg.id' )->join ( 'INNER JOIN', 'friends', ' msg.userid =friends.friendid and msg.userid = :id ', [':id' => Yii::$app->user->id ]);
-		$data = Message::find ()->select ( 'msg.id' )->join ( 'INNER JOIN', 'friends', ' msg.userid =friends.friendid and msg.userid = :id ', [':id' => $phone['id'] ]);
+		$data = Yii::$app->request->post ();
+		$phone = User::findOne ( [ 
+				'phone' => $data ['phone'] 
+		] );
+		// $data = Message::find ()->select ( 'msg.id' )->join ( 'INNER JOIN', 'friends', ' msg.userid =friends.friendid and msg.userid = :id ', [':id' => Yii::$app->user->id ]);
+		$data = Message::find ()->select ( 'msg.id' )->join ( 'INNER JOIN', 'friends', ' msg.userid =friends.friendid and msg.userid = :id ', [ 
+				':id' => $phone ['id'] 
+		] );
 		$pages = new \yii\data\Pagination ( [ 
 				'totalCount' => $data->count (),
 				'pageSize' => '10' 
 		] );
-		$models = $data->orderBy("msg.created_at desc")->offset ( $pages->offset )->limit ( $pages->limit )->all ();
+		$models = $data->orderBy ( "msg.created_at desc" )->offset ( $pages->offset )->limit ( $pages->limit )->all ();
 		$result = array ();
-		$result['item']=array ();
+		$result ['item'] = array ();
 		foreach ( $models as $model ) {
-			$msg = (new \yii\db\Query())->select(['msg.*','user.nickname','user.thumb'])->from ('msg')->join('INNER JOIN', 'user','msg.userid = user.id and msg.id = :id',[':id'=>$model['id']])->one ();
-			$info=$msg;
-			$info['apps'] = (new \yii\db\Query())->
-			select ( [ 
+			$msg = (new \yii\db\Query ())->select ( [ 
+					'msg.*',
+					'user.nickname',
+					'user.thumb' 
+			] )->from ( 'msg' )->join ( 'INNER JOIN', 'user', 'msg.userid = user.id and msg.id = :id', [ 
+					':id' => $model ['id'] 
+			] )->one ();
+			$info = $msg;
+			$info ['apps'] = (new \yii\db\Query ())->select ( [ 
 					'msgtoapp.appid',
 					'app.icon' 
-			] )->from ( 'msgtoapp' )->join ( 'INNER JOIN', 'app', 'msgtoapp.appid = app.id and msgtoapp.msgid = :id',[':id'=>$model ['id']])->all();
-			$info['replys'] = (new \yii\db\Query())->select(['reply.*','user1.nickname as fromnickname','user2.nickname as tonickname'])->from ( 'reply' )->join('INNER JOIN','user user1','user1.id = reply.fromid and reply.msgid= :id',[':id'=>$model ['id'] ])->join('Left JOIN','user user2','user2.id = reply.toid')->orderBy("reply.created_at")->all();
-			$result['item'][]=$info;
+			] )->from ( 'msgtoapp' )->join ( 'INNER JOIN', 'app', 'msgtoapp.appid = app.id and msgtoapp.msgid = :id', [ 
+					':id' => $model ['id'] 
+			] )->all ();
+			$info ['replys'] = (new \yii\db\Query ())->select ( [ 
+					'reply.*',
+					'user1.nickname as fromnickname',
+					'user2.nickname as tonickname' 
+			] )->from ( 'reply' )->join ( 'INNER JOIN', 'user user1', 'user1.id = reply.fromid and reply.msgid= :id', [ 
+					':id' => $model ['id'] 
+			] )->join ( 'Left JOIN', 'user user2', 'user2.id = reply.toid' )->orderBy ( "reply.created_at" )->all ();
+			$result ['item'] [] = $info;
 		}
 		$result ['_meta'] = array (
 				'pageCount' => $pages->pageCount,
@@ -82,92 +113,119 @@ class MessageController extends ActiveController {
 		return $result;
 		// return $model;
 	}
-	public function actionSend(){
+	public function actionSend() {
 		$data = Yii::$app->request->post ();
-		$msg = new Message();
-		$phone=User::findOne([
-				'phone'=>$data['phone']
-		]);
-		//$msg->userid = Yii::$app->user->id;
-		$msg->userid=$phone['id'];
-		$msg->content = $data['content'];
-		$msg->status=$data['status'];
-		$msg->created_at = time();
-		$err=$msg->save();
-		if($err==false){
-			throw new \yii\web\HttpException(404,"msg recode insert error");
+		$msg = new Message ();
+		$phone = User::findOne ( [ 
+				'phone' => $data ['phone'] 
+		] );
+		// $msg->userid = Yii::$app->user->id;
+		$msg->userid = $phone ['id'];
+		$msg->content = $data ['content'];
+		$msg->status = $data ['status'];
+		$msg->created_at = time ();
+		$err = $msg->save ();
+		if ($err == false) {
+			throw new \yii\web\HttpException ( 404, "msg recode insert error" );
 		}
-		foreach ($data['apps'] as $app) {
-			//echo $app;
-			$msgtoapp=new Msgtoapp();
-			$msgtoapp->msgid=$msg->id;
-			$msgtoapp->appid = $app['id'];
+		foreach ( $data ['apps'] as $app ) {
+			// echo $app;
+			$msgtoapp = new Msgtoapp ();
+			$msgtoapp->msgid = $msg->id;
+			$msgtoapp->appid = $app ['id'];
 			
-			$err=$msgtoapp->save();
-			if($err==false){
-				throw new \yii\web\HttpException(404,"msgtoapp recode insert error");
+			$err = $msgtoapp->save ();
+			if ($err == false) {
+				throw new \yii\web\HttpException ( 404, "msgtoapp recode insert error" );
 			}
 		}
-		return $data['apps'];
+		return $data ['apps'];
 	}
-	public function actionDeletemsg(){
-		$data = Yii::$app->request->post();
-		$id = $data['id'];
-		$msg = new Message();
-		$msg = Message::find()->where(['id'=>$id])->one();
-		if($msg == null){
-			//throw new \yii\web\NotFoundHttpException("record not found",401);
-			throw new \yii\web\HttpException(404,"recode not found");
-			//return "no record";
+	public function actionDeletemsg() {
+		$data = Yii::$app->request->post ();
+		$id = $data ['id'];
+		$msg = new Message ();
+		$msg = Message::find ()->where ( [ 
+				'id' => $id 
+		] )->one ();
+		if ($msg == null) {
+			// throw new \yii\web\NotFoundHttpException("record not found",401);
+			throw new \yii\web\HttpException ( 404, "recode not found" );
+			// return "no record";
 		}
-		//$msg->id = $id;
-		$err=$msg->delete();
-		if($err==false){
-			throw new \yii\web\HttpException(404,"recode delete error");
-		}else{
+		// $msg->id = $id;
+		$err = $msg->delete ();
+		if ($err == false) {
+			throw new \yii\web\HttpException ( 404, "recode delete error" );
+		} else {
 			echo json_encode ( array (
 					'flag' => 1,
 					'msg' => 'Delete success!' 
 			) );
 		}
 	}
-// 	public function actionIndex() {
-// 		// echo Yii::$app->user->id;
-// 		$msg = Message::find ()->select ( 'msg.id' )->join ( 'INNER JOIN', 'friends', 'friends.friendid = msg.userid' )->where ( 'friends.myid=:id', [ 
-// 				':id' => Yii::$app->user->id 
-// 		] )->orderBy ( 'msg.created_at' )->all ();
-// 		// return $msg;
-// 		$activeData = new ActiveDataProvider ( [ 
-// 				'query' => Message::find ()->where ( 'userid=:id', [ 
-// 						':id' => Yii::$app->user->id 
-// 				] ),
-// 				'pagination' => [ 
-// 						'defaultPageSize' => 10 
-// 				] 
-// 		] );
-// 		return $activeData;
-// 	}
-// 	public function actionShow() {
-// 		$msgs = Message::find ()->all ();
-// 		return $msgs;
-// 	}
-// 	public function actionHello() {
-// 		$response = Yii::$app->response;
-// 		$response->format = \yii\web\Response::FORMAT_JSON;
-// 		return "hello";
-// 	}
-	
-	public function actionReply(){
+	// public function actionIndex() {
+	// // echo Yii::$app->user->id;
+	// $msg = Message::find ()->select ( 'msg.id' )->join ( 'INNER JOIN', 'friends', 'friends.friendid = msg.userid' )->where ( 'friends.myid=:id', [
+	// ':id' => Yii::$app->user->id
+	// ] )->orderBy ( 'msg.created_at' )->all ();
+	// // return $msg;
+	// $activeData = new ActiveDataProvider ( [
+	// 'query' => Message::find ()->where ( 'userid=:id', [
+	// ':id' => Yii::$app->user->id
+	// ] ),
+	// 'pagination' => [
+	// 'defaultPageSize' => 10
+	// ]
+	// ] );
+	// return $activeData;
+	// }
+	// public function actionShow() {
+	// $msgs = Message::find ()->all ();
+	// return $msgs;
+	// }
+	// public function actionHello() {
+	// $response = Yii::$app->response;
+	// $response->format = \yii\web\Response::FORMAT_JSON;
+	// return "hello";
+	// }
+	public function actionReply() {
 		$data = Yii::$app->request->post ();
-		$model=new Reply();
-		$model->fromid=$data['fromid'];
-		$model->toid=$data['toid'];
-		$model->msgid=$data['msgid'];
-		$model->content=$data['content'];
-		$model->isread=false;
-		$model->created_at=time();
-		$model->save();
+		$model = new Reply ();
+		$model->fromid = $data ['fromid'];
+		$model->toid = $data ['toid'];
+		$model->msgid = $data ['msgid'];
+		$model->content = $data ['content'];
+		$model->isread = false;
+		$model->created_at = time ();
+		$model->save ();
 	}
-	
-	
+	public function actionPush() {
+		$sdk = new PushSDK ();
+		$channelId = '4483825412066692748';
+		$message = array (
+				// 消息的标题.
+				'title' => 'Hi!.',
+				// 消息内容
+				'description' => "hello!, this message from baidu push service." 
+		);
+		
+		// 设置消息类型为 通知类型.
+		$opts = array (
+				'msg_type' => 1 
+		);
+		
+		// 向目标设备发送一条消息
+		$rs = $sdk->pushMsgToSingleDevice ( $channelId, $message, $opts );
+		
+		// 判断返回值,当发送失败时, $rs的结果为false, 可以通过getError来获得错误信息.
+		if ($rs === false) {
+			print_r ( $sdk->getLastErrorCode () );
+			print_r ( $sdk->getLastErrorMsg () );
+		} else {
+			// 将打印出消息的id,发送时间等相关信息.
+			print_r ( $rs );
+		}
+		echo "done!";
+	}
 }
