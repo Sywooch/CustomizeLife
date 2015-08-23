2596c5afa2075f302c5c56f55645b76603274c24
jiaweizhou <jiaweizhou@zju.edu.cn>
2015-08-23 15:15:43 +0800

    no

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=customlife',
+    'dsn' => 'mysql:host=10.10.105.230;dbname=customlife',
     'username' => 'root',
-    'password' => '',
+    'password' => '123456',
     'charset' => 'utf8',
 ];
