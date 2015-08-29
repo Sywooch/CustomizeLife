6c38680c3bead5d70ff1563c5c708dabb6f49e05
jiaweizhou <jiaweizhou@zju.edu.cn>
2015-08-29 22:04:04 +0800

    add jumps from msg to app

---
+++ views/msgtoapp/_form_msg.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\v1\models\Msgtoapp */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="msgtoapp-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+    <?= $form->field($model, 'appid')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? '创建' : '更新', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
