dad80810492f4f5bee3c823250af9059e6d16b1f
JackXuF <xufei@xufeiMac.local>
2015-10-22 23:39:50 +0800

    modify tag

--- modules/v1/controllers/MyappController.php
+++ modules/v1/controllers/MyappController.php
@@ -12,40 +12,68 @@ use app;
 use app\modules\v1\models;
 use yii\filters\AccessControl;
 class MyappController extends Controller {
+// 	public function actionDownload() {
+// 		// \Yii::$app->response->format=\yii\web\Response::FORMAT_JSON;
+// 		$usertoapp = new Usertoapp ();
+// 		// $user=new User();
+// 		$appl = new Appl ();
+// 		$data = Yii::$app->request->post ();
+// 		if ($usertoapp->find ()->where ( [ 
+// 				'userid' => $data ['userid'],
+// 				'appid' => $data ['appid'] 
+// 		] )->one ()) {
+// 			echo json_encode ( array (
+// 					'flag' => 0,
+// 					'msg' => 'Already download!' 
+// 			) );
+// 			exit ();
+// 		} else {
+// 			/*$connection = \Yii::$app->db;
+// 			$command = $connection->createCommand ( 'UPDATE app SET downloadcount=downloadcount+1 WHERE id=' . $data ['appid'] );
+// 			$command->execute ();*/
+// // 			$appinfo = $appl->find ()->where ( [ 
+// // 					'id' => $data ['appid'] 
+// // 			] )->one ();
+// 			$appinfo=Appl::findOne([
+// 					'id' => $data ['appid']
+// 			]);
+// 			$usertoapp->appid = $data ['appid'];
+// 			$usertoapp->userid = $data ['userid'];
+// 			$usertoapp->created_at = time ();
+// 			$usertoapp->save ();
+// 			$appinfo['downloadcount']=$appinfo['downloadcount']+1;
+// 			$appinfo->save();
+// 			return $appinfo;
+// 		}
+// 	}
+	
 	public function actionDownload() {
-		// \Yii::$app->response->format=\yii\web\Response::FORMAT_JSON;
-		$usertoapp = new Usertoapp ();
-		// $user=new User();
 		$appl = new Appl ();
 		$data = Yii::$app->request->post ();
-		if ($usertoapp->find ()->where ( [ 
-				'userid' => $data ['userid'],
-				'appid' => $data ['appid'] 
-		] )->one ()) {
+		if (isset($data['phone'])){
+			$userinfo=User::findOne([
+					'phone'=>$data['phone']
+			]);
+			$userinfo['shared']+=1;
+			$userinfo->save();
+		} 
+		$appinfo=Appl::findOne([
+				'id' => $data ['appid']
+		]);
+		$appinfo['downloadcount']=$appinfo['downloadcount']+1;
+	if($appinfo->save()){
+			echo json_encode ( array (
+					'flag' => 1,
+					'msg' => 'Success!'
+			) );
+		}else{
 			echo json_encode ( array (
 					'flag' => 0,
-					'msg' => 'Already download!' 
+					'msg' => 'Failed!'
 			) );
-			exit ();
-		} else {
-			/*$connection = \Yii::$app->db;
-			$command = $connection->createCommand ( 'UPDATE app SET downloadcount=downloadcount+1 WHERE id=' . $data ['appid'] );
-			$command->execute ();*/
-// 			$appinfo = $appl->find ()->where ( [ 
-// 					'id' => $data ['appid'] 
-// 			] )->one ();
-			$appinfo=Appl::findOne([
-					'id' => $data ['appid']
-			]);
-			$usertoapp->appid = $data ['appid'];
-			$usertoapp->userid = $data ['userid'];
-			$usertoapp->created_at = time ();
-			$usertoapp->save ();
-			$appinfo['downloadcount']=$appinfo['downloadcount']+1;
-			$appinfo->save();
-			return $appinfo;
 		}
 	}
+	
 	public function actionDelete() {
 		$usertoapp = new Usertoapp ();
 		$data = Yii::$app->request->post ();
